3aba89235293da50121cb80ffe484228168c2644
Mehdi <elwafi.courrier99@gmail.com>
2022-07-16 17:25:58 +0100

    Added proxmox configuration variables

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,7 +1,9 @@
 terraform {
+  required_version = ">= 0.13.0"
+
   required_providers {
     proxmox = {
-      source = "Telmate/proxmox"
+      source  = "Telmate/proxmox"
       version = "2.9.10"
     }
   }
