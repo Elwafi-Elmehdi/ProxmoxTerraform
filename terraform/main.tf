0bcfdca089206d7c6d7c4b2ac5b792198e70a48e
Mehdi <elwafi.courrier99@gmail.com>
2022-07-16 19:30:16 +0100

    Added vm-qemu resource to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,29 @@ provider "proxmox" {
   pm_tls_insecure     = true # to skip ca trust issues
   pm_debug            = true # to debug problem with the client api
 }
+
+resource "proxmox_vm_qemu" "ubuntu-test-1" {
+  name        = "ubuntu-2"
+  desc        = "Ubuntu test server terraform init"
+  vmid        = 201
+  target_node = "pve2"
+  agent       = 1
+  clone       = "ubuntu"
+  
+  sockets     = 1
+  cores       = 1
+  cpu         = "host"
+  memory      = 2048
+  pool        = "stagging"
+  network {
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+  disk {
+    storage = "local-lvm"
+    type    = "virtio"
+    size    = "8G"
+  }
+  os_type   = "cloud-init"
+  ipconfig0 = "ip=192.168.1.16/24,gw=192.168.1.254"
+}
