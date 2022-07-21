177703026e4dcd8ff59b56e5460ae5ab43e64c66
Mehdi <elwafi.courrier99@gmail.com>
2022-07-21 02:09:04 +0100

    Updated Infrastures added pool and vms soucers

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,22 +11,24 @@ resource "proxmox_pool" "proxmox_cluster_pool_prod" {
   comment = "a pool for all resources that are crucial and up runing"
 }
 
-resource "proxmox_vm_qemu" "ubuntu-1" {
-  name        = "ubuntu-1"
-  desc        = "Ubuntu test server terraform init"
-  vmid        = 101
-  target_node = "pve1"
-  agent       = 1
-  clone       = "ubuntu"
+resource "proxmox_pool" "proxmox_cluster_pool_testing" {
+  poolid  = "testing"
+  comment = "a pool for all temprorary resources"
+}
+
+resource "proxmox_vm_qemu" "centos-1" {
+  name        = "centos-1"
+  desc        = "CentOS Test Server for practicing my linux+ cours"
+  vmid        = 201
+  target_node = "pve2"
+  pool        = "testing"
+  iso         = "CentOS-7-x86_64-Minimal-2009.iso"
   oncreate    = true
   sockets     = 1
   cores       = 1
-  cpu         = "host"
-  memory      = 2048
-  full_clone  = true
-  os_type     = "cloud-init"
-  ipconfig0   = "ip=192.168.1.16/24,gw=192.168.1.254"
-  ciuser      = "mehdi"
+  memory      = 1024
+  os_type     = "Linux 5.x - 2.6 Kernel"
+
   network {
     bridge = "vmbr0"
     model  = "virtio"
@@ -34,35 +36,66 @@ resource "proxmox_vm_qemu" "ubuntu-1" {
   disk {
     storage = "local-lvm"
     type    = "scsi"
-    size    = "8G"
-  }
-}
-
-resource "proxmox_lxc" "docker8" {
-  vmid         = 304
-  clone        = "300"
-  full         = true
-  hostname     = "docker8"
-  target_node  = "pve2"
-  unprivileged = true
-  password     = var.proxmox_lxc_password
-  cores        = 1
-  memory       = 1024
-
-  features {
-    nesting = true
-  }
-
-  rootfs {
-    storage = "local-lvm"
     size    = "4G"
   }
+}
+
+
+resource "proxmox_vm_qemu" "ubuntu-1" {
+  name        = "ubuntu-1"
+  desc        = "Ubuntu Test Server for practicing my linux+ cours"
+  vmid        = 101
+  target_node = "pve1"
+  agent       = 1
+  pool        = "testing"
+  clone       = "ubuntu"
+  oncreate    = true
+  sockets     = 1
+  cores       = 1
+  memory      = 2048
+  full_clone  = true
+  os_type     = "Linux 5.x - 2.6 Kernel"
+  ipconfig0   = "ip=192.168.1.3/24,gw=192.168.1.254"
+  ciuser      = "mehdi"
+  sshkeys     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO4/yKDpcdKmYEG/qEM5cz+YSGWoSRWgtTf0BioU60D1 Ubuntu proxmox"
 
   network {
-    name   = "eth0"
     bridge = "vmbr0"
-    ip     = "192.168.1.17/24"
-    gw     = "192.168.1.254"
-    ip6    = "dhcp"
+    model  = "virtio"
+  }
+  disk {
+    storage = "local-lvm"
+    type    = "scsi"
+    size    = "4G"
   }
 }
+
+# resource "proxmox_lxc" "docker" {
+#   count = 3
+#   vmid = sum()
+#   clone        = "300"
+#   full         = true
+#   hostname     = "docker8"
+#   target_node  = "pve2"
+#   unprivileged = true
+#   password     = var.proxmox_lxc_password
+#   cores        = 1
+#   memory       = 1024
+
+#   features {
+#     nesting = true
+#   }
+
+#   rootfs {
+#     storage = "local-lvm"
+#     size    = "4G"
+#   }
+
+#   network {
+#     name   = "eth0"
+#     bridge = "vmbr0"
+#     ip     = "192.168.1.17/24"
+#     gw     = "192.168.1.254"
+#     ip6    = "dhcp"
+#   }
+# }
